bd70cb42f95efcec9a40f88e28c30dcea5ca31e1
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2022-10-14 22:18:13 +0300

    Added Tema3

---
+++ Tema3/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def f(x):
+    return 6*x**2 - 12*x - 1
+
+
+def df(x):
+    return 12*x - 12
+
+
+def gradient_descent(start_x, iteration, learning_rate):
+    data = list()
+    data.append(start_x)
+    for i in range(iteration):
+        data.append(data[i] - learning_rate * df(data[i]))
+        print(data[i] - learning_rate * df(data[i]))
+
+
+gradient_descent(-1, 100, 0.01)
