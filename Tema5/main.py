0fb49595ad6e8f247c8cb558244c9f1c62134dd4
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2022-11-06 07:44:53 +0200

    Modified Tema5

--- Tema5/main.py
+++ Tema5/main.py
@@ -3,13 +3,15 @@ import random
 import numpy as np
 
 
-def init_random_weights(p=3):
+def init_random_weights(p=2):
     w1 = []
     w2 = []
+    w3 = []
     for i in range(p):
         w1.append(random.randint(10, 200))
         w2.append(random.randint(10, 200))
-    return np.array([w1, w2])
+        w3.append(random.randint(10, 200))
+    return np.array([w1, w2, w3])
 
 
 def read_patterns(filepath: str):
@@ -22,14 +24,14 @@ def read_patterns(filepath: str):
     return np.array(data)
 
 
-def winner_takes_all(patterns, weights, epochs=20, learning_rate=1):
+def winner_takes_all(patterns, weights, epochs=20, learning_rate=0.1):
     for i in range(epochs):
         print(f'Epoch: {i+1}')
         for j in range(len(patterns)):
             min_from = list()
-            min_from.append(patterns[j] - weights[:, 0])
-            min_from.append(patterns[j] - weights[:, 1])
-            min_from.append(patterns[j] - weights[:, 2])
+            min_from.append(np.linalg.norm(patterns[j] - weights[0]))
+            min_from.append(np.linalg.norm(patterns[j] - weights[1]))
+            min_from.append(np.linalg.norm(patterns[j] - weights[2]))
             min_from = np.array(min_from)
             dct = {}
             minim = []
@@ -38,12 +40,13 @@ def winner_takes_all(patterns, weights, epochs=20, learning_rate=1):
                 minim.append(np.sum(val))
             minim = np.min(minim)
             index = [key for key, value in dct.items() if value == minim][0]
-            weights[:, index] = weights[:, index] + learning_rate*min_from[index]
-            print(f'p1: {weights[:, 0]}, p2: {weights[:, 1]}, p3: {weights[:, 2]} The prototype for pattern {j + 1}')
+            weights[index] = weights[index] + learning_rate*(patterns[j] - weights[index])
+            print(f'p1: {weights[0]}, p2: {weights[1]}, p3: {weights[2]} The prototype for pattern {j + 1}')
         print()
 
 
 w = init_random_weights()
+print(w)
 x = read_patterns('patterns.csv')
 
 winner_takes_all(x, w)
